3ff388b7afd93dbd3a46557b1c012389deb6e91a
vnnikolov30 <v.n.nikolov30@gmail.com>
2026-02-10 19:56:32 +0200

    added gallery with tabs and categories

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
   const t = useTranslations("HeroSection");
   return (
     <>
-      <section className="flex flex-col items-center justify-start px-6 lg:px-[8%] py-6 gap-6 h-[100vh] max-h-screen overflow-hidden">
+      <section className="flex flex-col items-center justify-start px-6 lg:px-[8%] py-6 gap-6 h-100vh max-h-screen overflow-hidden">
         
 
         <div className="w-full max-w-4x1 mx-auto relative rounded-4xl overflow-hidden">
@@ -20,7 +20,7 @@ function Header() {
           <div className="absolute inset-0 pointer-events-none bg-[radial-gradient(ellipse_at_center,_rgba(0,0,0,0)_0%,_rgba(0,0,0,0.4)_100%)] rounded-4xl" />
         </div>
 
-        <div className="w-full text-center flex flex-col justify-center flex-grow">
+        <div className="w-full text-center flex flex-col justify-center grow">
           <h1 className="text-4xl md:text-6xl font-bold mb-4">{t("h1")}</h1>
           <p className="text-lg text-gray-600 max-w-xl mx-auto font-russo_one">
             {t("p")}
